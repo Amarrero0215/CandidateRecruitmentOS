0100d006779032f54d348312bfa19592ca1b2fab
Alex Marrero <alexmarrero0215@gmail.com>
2025-01-28 22:08:11 -0500

    Rearranged config files and fixed some typescript issue that was occuring in my code

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  envDir: './env',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load environment variables correctly
+  const env = loadEnv(mode, path.resolve(process.cwd(), "environment")); 
+
+  return {
+    plugins: [react()],
+    define: {
+      "import.meta.env": env, 
+    },
+  };
 });
